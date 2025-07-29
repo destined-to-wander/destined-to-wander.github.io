1b624d7d847f9aac881fbea5b5d37910d48cf56c
DestinedToWander <100820991+destined-to-wander@users.noreply.github.com>
2025-07-30 07:39:09 +0800

    Added menu bar and transitions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Website
+
+filename: lab01_matthias_252704R_A2
--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 function showContent(contentID){
     var sections = document.querySelectorAll(".section")
     for (let section of sections){
-        section.style.display = "none";
-    }
-    
-    let activeSections = document.querySelectorAll(".section"+contentID);
-    for (let activeSection of activeSections){
-        activeSection.style.display="flex";
+        if (section.classList.contains("section"+contentID)){
+            section.style.display = "block";
+            section.classList.add("activeSection");
+        } else {
+            section.classList.remove("activeSection");
+            section.style.display = "none";
+        }
     }
 }
 
 function formatInternalNavBar(contentID){
     var sections = document.querySelectorAll(".navbar")
     for (let section of sections){
-        section.style.display = "none";
-    }
-    
-    let activeSections = document.querySelectorAll("#navbar"+contentID);
-    for (let activeSection of activeSections){
-        activeSection.style.display="grid";
+        if (section.id == "navbar" + contentID){
+            section.style.display="flex";
+        } else {
+            section.style.display = "none";
+        }
     }
 }
 
@@ -31,8 +31,19 @@ document.getElementById("history").addEventListener("click", function(){displayS
 document.getElementById("methods").addEventListener("click", function(){displaySection(2)})
 document.getElementById("environ").addEventListener("click", function(){displaySection(3)})
 
-function main(){
-    displaySection(1);
-}
+document.getElementById("external").addEventListener("click",function(evt){
+    console.log(this.contentID); //see who is sender
+})
 
-main();
+document.getElementById("internalHBMenu").addEventListener("click",function(evt){
+    document.getElementById("internal").style.right = "0"; //see who is sender
+})
+
+document.addEventListener("click", function(evt){
+    if (evt.target.id != "internal" && !document.getElementById("internal").contains(evt.target))
+        document.getElementById("internal").style.right = "-250px"; 
+})
+
+
+// main:
+displaySection(1);
